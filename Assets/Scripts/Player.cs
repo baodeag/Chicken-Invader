5a18264558d3f4345009fdd563e47cef4d897489
baodeag <tranvietbao0907@gmail.com>
2025-08-10 21:58:33 +0700

    Player movement and pathfinding for enemy

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float paddingLeft;
+    [SerializeField] float paddingRight;
+    [SerializeField] float paddingTop;
+    [SerializeField] float paddingBottom;
+
+    Vector2 rawInput;
+    Vector2 minBounds;
+    Vector2 maxBounds;
+
+    void Start()
+    {
+        InitBound();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void InitBound() // This method initializes the bounds of the camera view
+    {
+        Camera mainCamera = Camera.main;
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
+        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
+    }
+
+    void Move()
+    {
+        Vector2 delta = rawInput * moveSpeed * Time.deltaTime;
+        Vector2 newPos = new Vector2();
+        newPos.x = Mathf.Clamp(transform.position.x + delta.x , minBounds.x + paddingLeft, maxBounds.x - paddingRight); // Clamp the x position
+        newPos.y = Mathf.Clamp(transform.position.y + delta.y , minBounds.y + paddingBottom, maxBounds.y - paddingTop); // Clamp the y position
+        transform.position = newPos; 
+    }
+
+    void OnMove(InputValue value)
+    {
+        rawInput = value.Get<Vector2>();
+        Debug.Log("Raw Input: " + rawInput);
+    }
+
+}
